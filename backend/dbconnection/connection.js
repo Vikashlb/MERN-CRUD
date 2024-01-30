0cb81173c4fb05f23b6f5d797c557f7b5f3c1188
Vikash L B <114284891+Vikashlb@users.noreply.github.com>
2024-01-30 11:44:22 +0530

    Db connection Encrypted in connection.js

--- backend/dbconnection/connection.js
+++ backend/dbconnection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb+srv://vikash:vikash@mernlearning.alvysh4.mongodb.net/crud').then(()=>{
+mongoose.connect('mongodb+srv://<username>:<password>@mernlearning.alvysh4.mongodb.net/crud').then(()=>{
     console.log("DB Connection Successful")
 }).catch((err)=>{
     console.log(err)
